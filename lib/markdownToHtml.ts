b3450ac48c04ecfa510281b8910f74c392b6807b
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-07-19 00:37:01 +0900

    use gfm

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,10 +2,12 @@ import remark from "remark";
 import html from "remark-html";
 import math from "remark-math";
 import htmlKatex from "remark-html-katex";
+import gfm from "remark-gfm";
 
 export default async function markdownToHtml(markdownString: string) {
   const result = await remark()
     .use(html)
+    .use(gfm)
     .use(math)
     .use(htmlKatex)
     .process(markdownString);
