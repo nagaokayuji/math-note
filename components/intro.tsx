1c9d0da6da659b1781a0d367d0081d7dd8a41d27
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-07-19 00:00:47 +0900

    modify: font size

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,7 +1,7 @@
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="text-3xl md:text-3xl font-bold tracking-tighter leading-tight md:pr-8">
         Math note
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
