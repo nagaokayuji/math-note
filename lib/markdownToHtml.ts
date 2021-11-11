5b2b1552c81af9665deefbcead2e479d2257fb0b
nagaokayuji <62990385+nagaokayuji@users.noreply.github.com>
2021-11-11 18:30:00 +0900

    ✨

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -3,17 +3,13 @@ import html from "remark-html";
 import math from "remark-math";
 import htmlKatex from "remark-html-katex";
 import gfm from "remark-gfm";
-import rehypeKatex from "rehype-katex";
 
 export default async function markdownToHtml(markdownString: string) {
   const result = await remark()
     .use(html)
     .use(gfm)
-    // .use(slug)
-    // .use(toc, { heading: "目次", maxDepth: 2 })
     .use(math)
     .use(htmlKatex)
-    // .use(rehypeKatex)
     .process(markdownString);
   return result.toString();
 }
